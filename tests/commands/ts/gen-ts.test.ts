fef6b597a8a17984840ccea940f7bd68104eec61
Colin King <colin.king@segment.com>
2018-10-22 10:59:44 -0700

    Add missing awaits in tests

--- tests/commands/ts/gen-ts.test.ts
+++ tests/commands/ts/gen-ts.test.ts
@@ -2,5 +2,5 @@ import { genTS } from '../../../src/commands/gen-ts'
 import { testSnapshotSingleFile } from '../snapshots'
 
 test('genTS - compiled output matches snapshot', async () => {
-  testSnapshotSingleFile(events => genTS(events), 'index.ts')
+  await testSnapshotSingleFile(events => genTS(events), 'index.ts')
 })
