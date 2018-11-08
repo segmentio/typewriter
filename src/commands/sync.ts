9d68039c2dc43b5b05bc069648c1c3d44cfb777b
Colin King <colin.king@segment.com>
2018-11-08 11:26:34 -0800

    fix: prettier + better error messages

--- src/commands/sync.ts
+++ src/commands/sync.ts
@@ -5,6 +5,7 @@ import * as util from 'util'
 import { dirname } from 'path'
 import { getTrackingPlanFromNetwork } from '../lib/fetchPlan'
 import { preprocessRules } from '../lib/utils'
+import * as prettier from 'prettier'
 const writeFile = util.promisify(fs.writeFile)
 const stat = util.promisify(fs.stat)
 
@@ -44,20 +45,25 @@ async function validateParams(params: Params): Promise<Params> {
   const { trackingPlanId, workspaceSlug, token, outputPath } = params
 
   if (!/^rs_.*$/.test(trackingPlanId)) {
-    throw new Error(`Invalid trackingPlanId: '${trackingPlanId}'`)
+    throw new Error(`Invalid --trackingPlanId ('${trackingPlanId}')`)
   }
 
   if (workspaceSlug.length === 0) {
-    throw new Error(`Invalid workspaceSlug: '${workspaceSlug}'`)
+    throw new Error(`Invalid --workspaceSlug ('${workspaceSlug}')`)
   }
 
   // Two 43 character URL-safe (RFC 4648) base64 strings, separated by a period.
   if (!/^[a-zA-Z0-9-_=]{43}\.[a-zA-Z0-9-_=]{43}$/.test(token)) {
-    throw new Error(`Invalid token: '${token}'`)
+    throw new Error(`Invalid --token ('${token}')`)
   }
 
-  await stat(dirname(outputPath)).catch(err => {
-    throw new Error(`Invalid outputPath: '${outputPath}\n${err}'`)
+  await stat(dirname(outputPath)).catch(_ => {
+    throw new Error(`Invalid --outputPath: file directory doesn't exist ('${outputPath}')`)
+  })
+  await stat(outputPath).then(stats => {
+    if (stats.isDirectory()) {
+      throw new Error(`Invalid --outputPath: outputPath can't be a directory ('${outputPath}')`)
+    }
   })
 
   return params
@@ -68,7 +74,8 @@ export const handler = async (params: Params) => {
 
   const plan = await getTrackingPlanFromNetwork(workspaceSlug, trackingPlanId, token)
 
-  const output = JSON.stringify(preprocessRules(plan), null, 2)
+  const json = JSON.stringify(preprocessRules(plan))
+  const output = prettier.format(json, { parser: 'json' })
   await writeFile(outputPath, output)
 
   console.log(`\nSynced Tracking Plan (${plan.name}) to: ${outputPath}\n`)
