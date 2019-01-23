segmentio/typewriter

commit a5819f5be60e2e1770d25497590445cb819f9d93

fix: add integrations to node.js and support the AppsFlyer exception [SCH-1407] (#39)
This adds `integrations` to the Node.JS TypeScript declarations, along with `AppsFlyer` as an exception. This also adds `All` as a convenience.

src/commands/gen-js/typescript.ts
@@ -72,6 +72,19 @@ export interface TrackMessage<PropertiesType> {
    * https://segment.com/docs/spec/common/#context
    */
   context?: Object;
+  /**
+   * A dictionary of destination names that the message should be sent to.
+   * By default all destinations are enabled. 'All' is a special key that
+   * applies when no key for a specific destination is found.
+   * https://segment.com/docs/spec/common/#integrations
+   */
+  integrations?: {
+    All?: boolean
+    AppsFlyer?: {
+      appsFlyerId: string
+    }
+    [key: string]: boolean | { [key: string]: string }
+  }
 }`
 
 const ajsTopLevels = `export type AnalyticsJSCallback = () => void
@@ -82,7 +95,13 @@ export interface SegmentOptions {
    * Selectivly filter destinations. By default all destinations are enabled.
    * https://segment.com/docs/sources/website/analytics.js/#selecting-destinations
    */
-  integrations: { [key: string]: boolean }
+  integrations: {
+    All?: boolean
+    AppsFlyer?: {
+      appsFlyerId: string
+    }
+    [key: string]: boolean | { [key: string]: string }
+  }
 }`
 
 /** Target language for a.js TypeScript Declarations */

tests/commands/gen-js/__snapshots__/index.node.d.ts
@@ -44,6 +44,19 @@ export interface TrackMessage<PropertiesType> {
    * https://segment.com/docs/spec/common/#context
    */
   context?: Object;
+  /**
+   * A dictionary of destination names that the message should be sent to.
+   * By default all destinations are enabled. 'All' is a special key that
+   * applies when no key for a specific destination is found.
+   * https://segment.com/docs/spec/common/#integrations
+   */
+  integrations?: {
+    All?: boolean;
+    AppsFlyer?: {
+      appsFlyerId: string;
+    };
+    [key: string]: boolean | { [key: string]: string };
+  };
 }
 
 export interface The42_TerribleEventName3 {

examples/gen-js/js/analytics/generated/index.d.ts
@@ -6,7 +6,13 @@ export interface SegmentOptions {
    * Selectivly filter destinations. By default all destinations are enabled.
    * https://segment.com/docs/sources/website/analytics.js/#selecting-destinations
    */
-  integrations: { [key: string]: boolean };
+  integrations: {
+    All?: boolean;
+    AppsFlyer?: {
+      appsFlyerId: string;
+    };
+    [key: string]: boolean | { [key: string]: string };
+  };
 }
 
 export interface FeedViewed {

examples/gen-js/node/analytics/generated/index.d.ts
@@ -44,6 +44,19 @@ export interface TrackMessage<PropertiesType> {
    * https://segment.com/docs/spec/common/#context
    */
   context?: Object;
+  /**
+   * A dictionary of destination names that the message should be sent to.
+   * By default all destinations are enabled. 'All' is a special key that
+   * applies when no key for a specific destination is found.
+   * https://segment.com/docs/spec/common/#integrations
+   */
+  integrations?: {
+    All?: boolean;
+    AppsFlyer?: {
+      appsFlyerId: string;
+    };
+    [key: string]: boolean | { [key: string]: string };
+  };
 }
 
 export interface OrderCompleted {

examples/gen-js/ts/analytics/generated/index.d.ts
@@ -6,7 +6,13 @@ export interface SegmentOptions {
    * Selectivly filter destinations. By default all destinations are enabled.
    * https://segment.com/docs/sources/website/analytics.js/#selecting-destinations
    */
-  integrations: { [key: string]: boolean };
+  integrations: {
+    All?: boolean;
+    AppsFlyer?: {
+      appsFlyerId: string;
+    };
+    [key: string]: boolean | { [key: string]: string };
+  };
 }
 
 export interface FeedViewed {

tests/commands/gen-js/__snapshots__/index.d.ts
@@ -6,7 +6,13 @@ export interface SegmentOptions {
    * Selectivly filter destinations. By default all destinations are enabled.
    * https://segment.com/docs/sources/website/analytics.js/#selecting-destinations
    */
-  integrations: { [key: string]: boolean };
+  integrations: {
+    All?: boolean;
+    AppsFlyer?: {
+      appsFlyerId: string;
+    };
+    [key: string]: boolean | { [key: string]: string };
+  };
 }
 
 export interface The42_TerribleEventName3 {

commit d012b13c98d0c603d242803ca30bd24b2a23a2f0

fix: `strictNullCheck` TS errors integrations type (#43)
This PR fixes an issue seen by Typewriter-users where the optional types introduced in #39 cause `strictNullChecks` errors during builds:

```
integrations?: {
    All?: boolean
    AppsFlyer?: {
      appsFlyerId: string
    }
    // Adds `| undefined`
    [key: string]: boolean | { [key: string]: string } | undefined
}
```

```
yarn build
yarn run v1.6.0
$ make build
./node_modules/.bin/tsc && rsync -am --include='*.graphql' --include='*/' --exclude='*' src dist
src/lib/typewriter/generated/index.d.ts:54:5 - error TS2411: Property 'All' of type 'boolean | undefined' is not assignable to string index type 'boolean | { [key: string]: string; }'.

54     All?: boolean;
       ~~~

src/lib/typewriter/generated/index.d.ts:55:5 - error TS2411: Property 'AppsFlyer' of type '{ appsFlyerId: string; } | undefined' is not assignable to string index type 'boolean | { [key: string]: string; }'.

55     AppsFlyer?: {
       ~~~~~~~~~

Found 2 errors.
```

This PR also enables `strict` on this codebase.

examples/gen-js/ts/components/Layout.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 
-export default (props) => (
+const Layout: React.SFC<{}> = (props) => (
   <div>
     <Head>
       <meta name='viewport' content='width=device-width, initial-scale=1' />
@@ -15,3 +15,5 @@ export default (props) => (
     {props.children}
   </div>
 )
+
+export default Layout

examples/gen-js/ts/components/Modal.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Frame from './Frame'
 
-export default class extends React.Component<any> {
+interface Props {
+  id: string
+  onDismiss: () => void
+}
+
+export default class extends React.Component<Props> {
   private shim: any
   private photoWrap: any
 
   public render () {
     return (
-      <div ref={el => (this.shim = el)} className='shim' onClick={(e) => this.dismiss(e)}>
+      <div ref={el => (this.shim = el)} className='shim' onClick={(e: React.MouseEvent<HTMLDivElement>) => this.dismiss(e)}>
         <div ref={el => (this.photoWrap = el)} className='photo'>
           <Frame id={this.props.id} />
         </div>
@@ -32,7 +37,7 @@ export default class extends React.Component<any> {
     )
   }
 
-  private dismiss = (e) => {
+  private dismiss = (e: React.MouseEvent) => {
     if (this.shim === e.target ||
        this.photoWrap === e.target) {
       if (this.props.onDismiss) {

examples/gen-js/ts/pages/index.tsx
@@ -20,7 +20,7 @@ export default class extends React.Component<any> {
     Router.push('/')
   }
 
-  public showPhoto (e, id) {
+  public showPhoto (e: React.MouseEvent, id: string) {
     e.preventDefault()
     Router.push(`/?photoId=${id}`, `/photo?id=${id}`)
   }
@@ -39,12 +39,12 @@ export default class extends React.Component<any> {
               />
           }
           {
-            photos.map((id) => (
+            photos.map((id: string) => (
               <div key={id} className='photo'>
                 <a
                   className='photoLink'
                   href={`/photo?id=${id}`}
-                  onClick={(e) => this.showPhoto(e, id)}
+                  onClick={(e: React.MouseEvent<HTMLAnchorElement>) => this.showPhoto(e, id)}
                 >
                   {id}
                 </a>

src/commands/gen-android.ts
@@ -30,7 +30,7 @@ import {
 } from '../lib'
 import * as fs from 'fs'
 import * as util from 'util'
-import { map, camelCase, upperFirst } from 'lodash'
+import { get, map, camelCase, upperFirst } from 'lodash'
 import { AcronymStyleOptions } from 'quicktype-core/dist/support/Acronyms'
 
 const writeFile = util.promisify(fs.writeFile)
@@ -291,7 +291,7 @@ class AnalyticsJavaWrapperRenderer extends JavaRenderer {
       ],
       () => {
         const rawEventName = name
-          .proposeUnstyledNames(null)
+          .proposeUnstyledNames(new Map())
           .values()
           .next().value
         this.emitLine([
@@ -361,7 +361,7 @@ export async function genJava(
       $schema: rules.$schema || 'http://json-schema.org/draft-07/schema#',
       title: rules.title,
       description: rules.description,
-      ...rules.properties.properties
+      ...get(rules, 'properties.properties', {})
     }
 
     inputData.addSource(
@@ -373,8 +373,7 @@ export async function genJava(
 
   const lang = new AnalyticsJavaTargetLanguage(packageName, trackingPlan)
 
-  const files = await quicktypeMultiFile({ lang, inputData })
-  return files
+  return await quicktypeMultiFile({ lang, inputData })
 }
 
 export const handler = getTypedTrackHandler(async (params: Params, { events }) => {
@@ -382,11 +381,13 @@ export const handler = getTypedTrackHandler(async (params: Params, { events }) =
 
   if (params.language === 'java') {
     files = await genJava(events, params)
+  } else {
+    throw new Error(`Invalid language: ${params.language}`)
   }
 
   return Promise.all(
     map([...files.keys()], (fileName: string) => {
-      return writeFile(`${params.outputPath}/${fileName}`, files.get(fileName).lines.join('\n'))
+      return writeFile(`${params.outputPath}/${fileName}`, files.get(fileName)!.lines.join('\n'))
     })
   )
 })

src/commands/gen-ios.ts
@@ -25,7 +25,7 @@ import {
 import { version } from '../../package.json'
 import * as fs from 'fs'
 import * as util from 'util'
-import { map, camelCase, upperFirst } from 'lodash'
+import { map, camelCase, upperFirst, get } from 'lodash'
 
 const writeFile = util.promisify(fs.writeFile)
 
@@ -156,7 +156,7 @@ class AnalyticsObjectiveCWrapperRenderer extends ObjectiveCRenderer {
       this.emitLine('- (instancetype)initWithAnalytics:(SEGAnalytics *)analytics;')
       this.ensureBlankLine()
 
-      this.forEachTopLevel('leading-and-interposing', (c: ClassType, name: Name) => {
+      this.forEachTopLevel('leading-and-interposing', (c: Type, name: Name) => {
         this.emitDescription(this.descriptionForType(c))
         this.emitLine(['- (void)', this.variableNameForTopLevel(name), ':(', name, ' *)props;'])
         this.emitLine([
@@ -410,7 +410,7 @@ class AnalyticsObjectiveCWrapperRenderer extends ObjectiveCRenderer {
 
   protected rawName(name: Name) {
     return name
-      .proposeUnstyledNames(null)
+      .proposeUnstyledNames(new Map())
       .values()
       .next().value
   }
@@ -426,7 +426,7 @@ class AnalyticsObjectiveCWrapperRenderer extends ObjectiveCRenderer {
       })
       this.ensureBlankLine()
 
-      this.forEachTopLevel('leading-and-interposing', (_: ClassType, className: Name) => {
+      this.forEachTopLevel('leading-and-interposing', (_: Type, className: Name) => {
         this.emitAnalyticsWrapperMethod(className, { withOptions: false })
         this.emitAnalyticsWrapperMethod(className, { withOptions: true })
       })
@@ -562,7 +562,7 @@ export async function genObjC(events: TrackedEvent[], { trackingPlan, classPrefi
       $schema: rules.$schema || 'http://json-schema.org/draft-07/schema#',
       title: rules.title,
       description: rules.description,
-      ...rules.properties.properties
+      ...get(rules, 'properties.properties', {})
     }
 
     inputData.addSource(
@@ -583,11 +583,13 @@ export const handler = getTypedTrackHandler(async (params: Params, { events }) =
 
   if (params.language === 'objectivec') {
     files = await genObjC(events, params)
+  } else {
+    throw new Error(`Invalid language: ${params.language}`)
   }
 
   return Promise.all(
     map([...files.keys()], (fileName: string) => {
-      return writeFile(`${params.outputPath}/${fileName}`, files.get(fileName).lines.join('\n'))
+      return writeFile(`${params.outputPath}/${fileName}`, files.get(fileName)!.lines.join('\n'))
     })
   )
 })

src/commands/gen-js/index.ts
@@ -12,20 +12,20 @@ export const command = 'gen-js'
 export const desc = 'Generate a strongly typed JavaScript analytics.js client'
 
 export enum Client {
-  js = 0,
-  node = 1
+  js = 'js',
+  node = 'node'
 }
 
 export enum Declarations {
-  none = 0,
-  ts = 1
+  none = 'none',
+  ts = 'ts'
 }
 
-interface CompilerParams {
+interface Params extends DefaultParams {
   target?: ScriptTarget
   module?: ModuleKind
-  client?: string
-  declarations?: string
+  client: keyof typeof Client
+  declarations: keyof typeof Declarations
   runtimeValidation: boolean
 }
 
@@ -48,15 +48,15 @@ export const builder = {
   client: {
     type: 'string',
     required: false,
-    default: 'js',
-    choices: Object.keys(Client).filter(k => typeof Client[k as any] === 'number'),
+    default: Client.js,
+    choices: Object.keys(Client),
     description: 'Segment analytics library to generate for'
   },
   declarations: {
     type: 'string',
     required: false,
-    default: 'none',
-    choices: Object.keys(Declarations).filter(k => typeof Declarations[k as any] === 'number'),
+    default: Declarations.none,
+    choices: Object.keys(Declarations),
     description: 'Type declarations to generate alongside the JS library'
   },
   runtimeValidation: {
@@ -67,24 +67,22 @@ export const builder = {
   }
 }
 
-export const handler = getTypedTrackHandler(
-  async (params: DefaultParams & CompilerParams, { events }) => {
-    const files = []
+export const handler = getTypedTrackHandler(async (params: Params, { events }) => {
+  const files = []
 
-    const jsLibrary = await genJS(
-      events,
-      params.target,
-      params.module,
-      Client[params.client],
-      params.runtimeValidation
-    )
-    files.push(writeFile(`${params.outputPath}/index.js`, jsLibrary))
+  const jsLibrary = await genJS(
+    events,
+    params.target,
+    params.module,
+    Client[params.client],
+    params.runtimeValidation
+  )
+  files.push(writeFile(`${params.outputPath}/index.js`, jsLibrary))
 
-    if (Declarations[params.declarations] === Declarations.ts) {
-      const declarations = await genTSDeclarations(events, Client[params.client])
-      files.push(writeFile(`${params.outputPath}/index.d.ts`, declarations))
-    }
-
-    return Promise.all(files)
+  if (Declarations[params.declarations] === Declarations.ts) {
+    const declarations = await genTSDeclarations(events, Client[params.client])
+    files.push(writeFile(`${params.outputPath}/index.d.ts`, declarations))
   }
-)
+
+  return Promise.all(files)
+})

src/commands/gen-js/library.ts
@@ -57,9 +57,9 @@ export function genJS(
       const compiledValidationSource: any = ajv.compile(preprocessRules(rules)).source
       const compiledValidationFn = compiledValidationSource.code.replace(/return validate;/, '')
 
-      let parameters: string
-      let trackCall: string
-      let validateCall: string
+      let parameters = ''
+      let trackCall = ''
+      let validateCall = ''
       if (client === Client.js) {
         parameters = 'props, context'
         trackCall = `this.analytics.track('${name}', props, genOptions(context))`

src/commands/gen-js/typescript.ts
@@ -1,5 +1,5 @@
 import { TrackedEvent } from '../../lib'
-import { camelCase } from 'lodash'
+import { get, camelCase } from 'lodash'
 import * as prettier from 'prettier'
 
 import {
@@ -71,7 +71,7 @@ export interface TrackMessage<PropertiesType> {
    * A dictionary of extra context to attach to the call.
    * https://segment.com/docs/spec/common/#context
    */
-  context?: Object;
+  context?: any;
   /**
    * A dictionary of destination names that the message should be sent to.
    * By default all destinations are enabled. 'All' is a special key that
@@ -83,7 +83,7 @@ export interface TrackMessage<PropertiesType> {
     AppsFlyer?: {
       appsFlyerId: string
     }
-    [key: string]: boolean | { [key: string]: string }
+    [key: string]: boolean | { [key: string]: string } | undefined
   }
 }`
 
@@ -244,7 +244,7 @@ export async function genTSDeclarations(
       $schema: rules.$schema || 'http://json-schema.org/draft-07/schema#',
       title: rules.title,
       description: rules.description,
-      ...rules.properties.properties
+      ...get(rules, 'properties.properties', {})
     }
 
     inputData.addSource(

src/global.d.ts
@@ -1,0 +1,3 @@
+declare module 'omit-deep-lodash'
+declare module 'sort-keys'
+declare module '@nucleartide/dx'

src/lib/utils.ts
@@ -1,6 +1,6 @@
 import * as omitDeep from 'omit-deep-lodash'
 import * as sortKeys from 'sort-keys'
-import { flow } from 'lodash'
+import { get, flow } from 'lodash'
 
 /**
  * Remove all instances of `required: []` from a JSON Schema.
@@ -10,7 +10,7 @@ import { flow } from 'lodash'
  *
  * Inspired by: https://softwareengineering.stackexchange.com/a/323670
  */
-function removeEmptyRequireds(obj: object) {
+function removeEmptyRequireds(obj: any) {
   for (const property in obj) {
     if (obj.hasOwnProperty(property)) {
       const value = obj[property]
@@ -36,11 +36,11 @@ function removeEmptyRequireds(obj: object) {
 export const preprocessRules = flow(
   // In JSON Schema Draft-04, required fields must have at least one element.
   // Therefore, we strip `required: []` from your rules so this error isn't surfaced.
-  rules => {
+  (rules: any) => {
     removeEmptyRequireds(rules)
     return rules
   },
   // Enforce a deterministic ordering to reduce verson control deltas.
-  rules => sortKeys(rules, { deep: true }),
-  rules => omitDeep(rules, 'id')
+  (rules: any) => sortKeys(rules, { deep: true }),
+  (rules: any) => omitDeep(rules, 'id')
 )

tests/commands/gen-js/__snapshots__/index.node.d.ts
@@ -43,7 +43,7 @@ export interface TrackMessage<PropertiesType> {
    * A dictionary of extra context to attach to the call.
    * https://segment.com/docs/spec/common/#context
    */
-  context?: Object;
+  context?: any;
   /**
    * A dictionary of destination names that the message should be sent to.
    * By default all destinations are enabled. 'All' is a special key that
@@ -55,7 +55,7 @@ export interface TrackMessage<PropertiesType> {
     AppsFlyer?: {
       appsFlyerId: string;
     };
-    [key: string]: boolean | { [key: string]: string };
+    [key: string]: boolean | { [key: string]: string } | undefined;
   };
 }
 