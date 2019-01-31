d99dad1778197710ad04feaf29d65096737beba1
Colin <me@colinking.co>
2019-01-31 15:11:37 -0800

    test: update tests

--- examples/gen-js/ts/analytics/generated/index.d.ts
+++ examples/gen-js/ts/analytics/generated/index.d.ts
@@ -120,7 +120,7 @@ export interface ProfileViewed {
  */
 
 // From https://github.com/epoberezkin/ajv/blob/0c31c1e2a81e315511c60a0dd7420a72cb181e61/lib/ajv.d.ts#L279
-interface AjvErrorObject {
+export interface AjvErrorObject {
   keyword: string;
   dataPath: string;
   schemaPath: string;
@@ -132,13 +132,13 @@ interface AjvErrorObject {
 }
 
 // An invalid event with its associated collection of validation errors.
-interface InvalidEvent {
+export interface InvalidEvent {
   eventName: string;
   validationErrors: AjvErrorObject[];
 }
 
 // Options to customize the runtime behavior of a Typewriter client.
-interface AnalyticsOptions {
+export interface AnalyticsOptions {
   onError(event: InvalidEvent): void;
 }
 
