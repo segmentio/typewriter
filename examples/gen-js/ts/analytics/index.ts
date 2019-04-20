6890aba446d85c79888f0cc9bacbc13b86f5f0d3
Colin <me@colinking.co>
2019-04-20 13:28:08 -0700

    Clear slate 💣

--- examples/gen-js/ts/analytics/index.ts
+++
@@ -1,9 +0,0 @@
-import Analytics from './generated'
-
-declare global {
-  interface Window {
-    analytics: any
-  }
-}
-
-export default Analytics
--- src/global.d.ts
+++
@@ -1,3 +0,0 @@
-declare module 'omit-deep-lodash'
-declare module 'sort-keys'
-declare module '@nucleartide/dx'
