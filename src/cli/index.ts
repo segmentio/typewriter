a4b9b824551bf627503de1a013eee4f855cd71e3
Colin <me@colinking.co>
2019-08-21 11:40:18 -0700

    Fix typewriter --help text
    It now shows the following:
    
    ```
    Usage: `npx typewriter [<cmd>] [args]`
    
    A compiler for generating strongly-typed analytics clients using a Segment
    Tracking Plan. Docs are available at: https://segment.com/docs/typewriter
    
    To get started, just run `npx typewriter init`.
    
    Commands:
      npx typewriter init    Initializes a new typewriter project
                                                               [aliases: initialize]
      npx typewriter update  Syncs your tracking plans and re-generates a
                             development build of your client [default] [aliases: u]
      npx typewriter build   Generates a development build of your client
                                                   [aliases: b, d, dev, development]
      npx typewriter prod    Generates a production build of your client
                                                            [aliases: p, production]
      npx typewriter token   Prints your Segment API token              [aliases: t]
    
    Options:
      --help     Show help                                                 [boolean]
      --version  Show version number                                       [boolean]
      --config   Optional path to a typewriter.yml config   [string] [default: "./"]
    ```

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,61 +1,66 @@
 #!/usr/bin/env node
 
-import * as yargs from 'yargs'
 import { init, prod, update, token, build } from './commands'
 import { version } from '../../package.json'
 import { withAnalytics } from './analytics'
+import { Arguments, Config } from './types'
 
 const args = {
 	config: {
 		type: 'string' as 'string',
-		describe: 'optional path to a typewriter.yml config',
+		describe: 'Optional path to a typewriter.yml config ',
 		default: './',
 	},
 }
 
-yargs
-	.scriptName('typewriter')
-	.epilog('Further Typewriter documentation is available at: https://segment.com/docs/typewriter/')
-	.command(
-		['*', 'update'],
-		'A compiler for generating strongly typed analytics clients via Segment Protocols.',
-		args,
-		withAnalytics(update)
+// The '*' command is a catch-all. We want to fail the CLI if an unknown command is
+// supplied ('yarn typewriter footothebar'), instead of just running the default command.
+const withVerifyIsDefault = (f: (args: Arguments, cfg: Config | undefined) => Promise<void>) => {
+	return async (args: Arguments, cfg: Config | undefined) => {
+		if (args._.length === 0 || ['update', 'u'].includes(args._[0])) {
+			return f(args, cfg)
+		} else {
+			throw new Error(`Unknown command: '${args._[0]}'`)
+		}
+	}
+}
+
+require('yargs')
+	.showHelpOnFail(false)
+	.scriptName('npx typewriter')
+	.usage(
+		'Usage: `$0 [<cmd>] [args]`\n\nA compiler for generating strongly-typed analytics clients using a Segment Tracking Plan. Docs are available at: https://segment.com/docs/typewriter\n\nTo get started, just run `npx typewriter init`.'
 	)
 	.command(
 		['init', 'initialize'],
-		'initializes a new typewriter project',
+		'Initializes a new typewriter project',
 		args,
 		withAnalytics(init)
 	)
 	.command(
-		['b', 'build', 'd', 'dev', 'development'],
-		'generates a development build of your client',
+		['update', 'u', '*'],
+		'Syncs your tracking plans and re-generates a development build of your client',
+		args,
+		withAnalytics(withVerifyIsDefault(update))
+	)
+	.command(
+		['build', 'b', 'd', 'dev', 'development'],
+		'Generates a development build of your client',
 		args,
 		withAnalytics(build)
 	)
 	.command(
-		['p', 'prod', 'production'],
-		'generates a production build of your client',
+		['prod', 'p', 'production'],
+		'Generates a production build of your client',
 		args,
 		withAnalytics(prod)
 	)
-	.command(
-		['u', 'update'],
-		"update the event specs in your `plan.json`'s",
-		args,
-		withAnalytics(update)
-	)
-	.command(
-		['t', 'token'],
-		'prints the currently configured Segment API token',
-		args,
-		withAnalytics(token)
-	)
+	.command(['token', 't'], 'Prints your Segment API token', args, withAnalytics(token))
 	.command({
 		command: 'version',
 		describe: false,
 		handler: withAnalytics(() => console.log(version)),
 	})
+	.strict(true)
 	.help()
 	.version().argv
