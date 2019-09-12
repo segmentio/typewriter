414a0d6600aadf742786e133e9c078e298ebd5fe
Colin <me@colinking.co>
2019-09-11 20:29:11 -0700

    add wait to cypress tests

--- tests/e2e/web-typescript/cypress/integration/suite.js
+++ tests/e2e/web-typescript/cypress/integration/suite.js
@@ -3,4 +3,6 @@ const PORT = 3000
 it('Fire the standard Typewriter test suite', function() {
 	cy.visit(`localhost:${PORT}`)
 	expect(true).to.equal(true)
+	// Wait for analytics.js requests to finish.
+	cy.wait(2000)
 })
