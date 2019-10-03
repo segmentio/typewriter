4ba61ee59af3415c6216f00e862dcf5246d451e7
Colin <me@colinking.co>
2019-10-03 16:41:50 -0700

    v7.0.0-42

--- src/analytics/index.ts
+++ src/analytics/index.ts
@@ -152,7 +152,7 @@ function withTypewriterContext<P, T extends Segment.TrackMessage<P>>(
 			...(message.context || {}),
 			typewriter: {
 				language: 'typescript',
-				version: '7.0.0-41',
+				version: '7.0.0-42',
 			},
 		},
 	}
--- tests/e2e/ios/TypewriterExample/Analytics/SEGTypewriterUtils.m
+++ tests/e2e/ios/TypewriterExample/Analytics/SEGTypewriterUtils.m
@@ -14,7 +14,7 @@
   NSDictionary<NSString *, id> *typewriterContext = @{
     @"typewriter": @{
       @"language": @"objective-c",
-      @"version": @"7.0.0-41"
+      @"version": @"7.0.0-42"
     }
   };
   NSMutableDictionary *context = [NSMutableDictionary dictionaryWithCapacity:customContext.count + typewriterContext.count];
