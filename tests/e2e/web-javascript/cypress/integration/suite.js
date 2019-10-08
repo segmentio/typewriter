6d0ffb6bf8707a80676dbf32282bcc2a4eeabad5
Colin <me@colinking.co>
2019-10-07 20:46:45 -0700

    Tentatively fix web CI flakiness

--- tests/e2e/web-javascript/cypress/integration/suite.js
+++ tests/e2e/web-javascript/cypress/integration/suite.js
@@ -1,8 +1,19 @@
 const PORT = 3000
+const NUM_TRACK_CALLS = 21
 
 it('Fire the standard Typewriter test suite', function() {
+	cy.server()
+	cy.route({
+		method: 'POST',
+		url: 'http://localhost:8765/v1/t',
+	}).as('trackCall')
+
 	cy.visit(`http://localhost:${PORT}`)
+
 	// Wait for analytics.js requests to finish.
-	cy.wait(2000)
+	for (let i = 0; i < NUM_TRACK_CALLS; i++) {
+		cy.wait('@trackCall')
+	}
+
 	expect(true).to.equal(true)
 })
