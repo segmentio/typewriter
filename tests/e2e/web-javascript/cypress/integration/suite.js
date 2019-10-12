b1a79c1a4e650d330573344b2a5740caa9b1bb6b
Colin <me@colinking.co>
2019-10-11 18:23:17 -0700

    couple hacks

--- tests/e2e/web-javascript/cypress/integration/suite.js
+++ tests/e2e/web-javascript/cypress/integration/suite.js
@@ -8,12 +8,15 @@ it('Fire the standard Typewriter test suite', function() {
 		url: 'http://localhost:8765/v1/t',
 	}).as('trackCall')
 
+	cy.wait(500)
+
 	cy.visit(`http://localhost:${PORT}`)
 
+	cy.wait(500)
+
 	// Wait for analytics.js requests to finish.
-	for (let i = 0; i < NUM_TRACK_CALLS; i++) {
-		cy.wait('@trackCall')
-	}
+	const resp = cy.wait(Array(NUM_TRACK_CALLS).fill().map(_ => '@trackCall'))
+	console.log('track call fired: ', resp)
 
 	expect(true).to.equal(true)
 })
