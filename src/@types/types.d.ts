68ef47b2f1adad4ffbcf7630bcfb71c0ec78da2a
Colin <me@colinking.co>
2019-12-04 21:20:24 -0800

    Don't git add -A in precommit hook, so that we can RFPR

--- src/@types/types.d.ts
+++ src/@types/types.d.ts
@@ -3,6 +3,7 @@
  *
  * Based on ReturnType<T> from TypeScript.
  */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 type AsyncReturnType<T extends (...args: any) => any> = T extends (...args: any) => Promise<infer R>
 	? R
 	: any
