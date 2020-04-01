a5f981a0438df7feac0e46f1ce8ab4258aad1cea
Carlos Kelly <me@carlos.dev>
2020-04-01 12:54:34 -0500

    Added button with message.

--- tests/e2e/android-java/app/src/main/java/com/segment/TypewriterJavaExample/MainActivity.java
+++ tests/e2e/android-java/app/src/main/java/com/segment/TypewriterJavaExample/MainActivity.java
@@ -1,5 +1,7 @@
 package com.segment.TypewriterJavaExample;
 
+import android.view.View;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.segment.analytics.Analytics;
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
+
+  public void sendMessageButtonSelected(View view) {
+    EditText productNameEditText = findViewById(R.id.productNameEditText);
+    String productName = productNameEditText.getText().toString();
   }
 }
