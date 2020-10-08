f3083962904e27bf7395047ea05bf5ed94b3f523
Brandon Sneed <brandon.sneed@segment.com>
2020-10-08 15:30:32 -0700

    removed window reference to get back to known behavior

--- tests/e2e/ios-swift/TypewriterSwiftExample/AppDelegate.swift
+++ tests/e2e/ios-swift/TypewriterSwiftExample/AppDelegate.swift
@@ -15,8 +15,6 @@ import Analytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         Analytics.debug(true)
         
