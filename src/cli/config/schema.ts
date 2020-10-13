43bb1d172e04e554fdf503fac0732966081c7add
Bryant Biggs <bryantbiggs@gmail.com>
2020-10-13 19:13:02 -0400

    fix: update joi schema validation

--- src/cli/config/schema.ts
+++ src/cli/config/schema.ts
@@ -82,10 +82,10 @@ const ConfigSchema = Joi.object().required().keys({
 
 export const validateConfig = (rawConfig: object): Config => {
 	// Validate the provided configuration file using our Joi schema.
-	const result = Joi.validate(rawConfig, ConfigSchema, {
-		abortEarly: false,
-		convert: false,
-	})
+	const result = ConfigSchema.validate(rawConfig, {
+    abortEarly: false,
+    convert: false,
+  });
 	if (!!result.error) {
 		throw new Error(result.error.annotate())
 	}
