37260f0cb1a109b7717a653317d3fefb5cf0e4c7
Alan Charles <alan.charles@segment.com>
2021-07-09 12:48:20 -0600

    Fix: update types and lodash flow func

--- src/cli/api/trackingplans.ts
+++ src/cli/api/trackingplans.ts
@@ -41,10 +41,10 @@ export async function writeTrackingPlan(
 	await verifyDirectoryExists(path, 'file')
 
 	// Perform some pre-processing on the Tracking Plan before writing it.
-	const planJSON = flow<SegmentAPI.TrackingPlan, SegmentAPI.TrackingPlan, string>(
+	const planJSON = flow<[SegmentAPI.TrackingPlan], SegmentAPI.TrackingPlan, string>(
 		// Enforce a deterministic ordering to reduce verson control deltas.
-		plan => sanitizeTrackingPlan(plan),
-		plan => stringify(plan, { space: '\t' })
+		(plan: SegmentAPI.TrackingPlan) => sanitizeTrackingPlan(plan),
+		(plan: SegmentAPI.TrackingPlan) => stringify(plan, { space: '\t' })
 	)(plan)
 
 	await writeFile(path, planJSON, {
