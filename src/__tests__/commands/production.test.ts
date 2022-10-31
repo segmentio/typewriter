4ab680c1277d69e57204f61bb2e221e1d593e06e
Oscar Bazaldua <511911+oscb@users.noreply.github.com>
2022-10-31 15:39:56 -0600

    fix: add AJV imports in node and ajs templates

---
+++ src/__tests__/commands/production.test.ts
@@ -0,0 +1,35 @@
+import fs from "node:fs";
+import path from "node:path";
+
+import { setupEnv } from "../__helpers__/environment";
+import { run } from "../__helpers__/oclif-runner";
+
+const TEST_ENV_PATH = "production";
+
+describe("prod", () => {
+  const configurations = [
+    ["typescript", "analytics-node", "segment.ts"],
+    ["typescript", "analytics-js", "segment.ts"],
+  ];
+
+  for (const config of configurations) {
+    const [language, sdk, filename, plan, id, outputPath, legacyId] = config;
+    it(`builds production client Language: ${language}, SDK:${sdk}`, async () => {
+      const testPath = await setupEnv(
+        TEST_ENV_PATH,
+        language,
+        sdk,
+        plan,
+        id,
+        outputPath,
+        legacyId
+      );
+      await run(["production", "-c", testPath]);
+      expect(
+        fs.readFileSync(path.join(testPath, filename), {
+          encoding: "utf-8",
+        })
+      ).toMatchSnapshot();
+    });
+  }
+});
