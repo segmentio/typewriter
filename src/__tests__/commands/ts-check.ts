2ded1bc3cec51e42f44a4921dbea62bdc70ba524
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-04-24 17:06:41 -0500

    wip

--- src/__tests__/commands/ts-check.ts
+++
@@ -1,5 +0,0 @@
-import { AnalyticsBrowser, AnalyticsSnippet } from '@segment/analytics-next';
-import typewriter from '../runtime-tests/fixtures/typescript-analytics-js'
-
-typewriter.setTypewriterOptions({ analytics: {} as AnalyticsBrowser });
-typewriter.setTypewriterOptions({ analytics: {} as AnalyticsSnippet});
